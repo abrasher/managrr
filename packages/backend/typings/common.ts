e0b063c5f0979e5d1d5aa0e6b46418be6ddccf32
Amber <amber.m.brasher@gmail.com>
2021-05-24 20:23:55 -0400

    fixed imports

--- packages/backend/typings/common.ts
+++ packages/backend/typings/common.ts
@@ -1,6 +1,6 @@
-import { OMDbMetadata } from '@/backend/backend/modules/importer/OMDbMetadata'
-import { PlexMetadata } from '@/backend/backend/modules/importer/PlexImporter'
-import { RadarrMetadata } from '@/backend/backend/modules/importer/RadarrImporter'
-import { TMDbMetadata } from '@/backend/backend/modules/importer/TMDbMetadata'
+import { OMDbMetadata } from '@/backend/modules/importer/OMDbMetadata'
+import { PlexMetadata } from '@/backend/modules/importer/PlexImporter'
+import { RadarrMetadata } from '@/backend/modules/importer/RadarrImporter'
+import { TMDbMetadata } from '@/backend/modules/importer/TMDbMetadata'
 
 export type { OMDbMetadata, PlexMetadata, RadarrMetadata, TMDbMetadata }
